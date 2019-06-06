f203011e42b2e91401253509648ec5fcb9b5eb94
Shu <shu.liang89@gmail.com>
2019-06-06 18:54:37 -0400

    login completed

--- src/App.js
+++ src/App.js
@@ -4,23 +4,43 @@ import {BrowserRouter as Router, Switch,Route} from 'react-router-dom'
 // import './App.css';
 
 import Home from './components/Home';
+import UserProfile from './components/UserProfile';
+import LogIn from './components/Login';
 
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      accountBalance: 14568.27
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: 'bob_loblaw',
+        memberSince: '08/23/99',
+      }
     }
+  }
+
+  mockLogIn = (logInInfo) => {
+    const newUser = {...this.state.currentUser}
+    newUser.userName = logInInfo.userName
+    this.setState({currentUser: newUser})
   }
 
 
   render(){
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (
+      <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
+    );
+
+    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} {...this.props}/>)
+
     return (
       <Router>
         <Switch>
           <Route exact path="/" render={HomeComponent}/>
+          <Route exact path="/userProfile" render={UserProfileComponent}/>
+          <Route exact path="/login" render={LogInComponent}/>
         </Switch>
       </Router>
     );
