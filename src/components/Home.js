d75aaff26b3625a198235a9eec539d765ed6c509
Shu <shu.liang89@gmail.com>
2019-06-06 18:35:48 -0400

    passing props to routes completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import AccountBalance from './AccountBalance';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
-        <h1>Bank of React</h1>
-      </div>
+        <div>
+          <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
+          <h1>Bank of React</h1>
+
+          <AccountBalance accountBalance={this.props.accountBalance}/>
+        </div>
     );
   }
 }
